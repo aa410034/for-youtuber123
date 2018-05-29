4969221c085b38fd4de9be84aa72cb7f0711d9df
rock <xx410034@gmail.com>
2018-05-29 23:15:42 +0800

    常非

--- Untitled2.c
+++ Untitled2.c
@@ -1,4 +1,4 @@
-void BS(int *a)
+void BS(const int *a,int *as)
 { 
 int i,j,temp=0;
 for(i=0;i<10;i++)
@@ -6,11 +6,11 @@ for(i=0;i<10;i++)
 
 	for(j=0;j<10;j++)
 	{
-		if(a[j]>a[i])
+		if(as[j]>as[i])
 		{
-		temp=a[j];
-		a[j]=a[i];
-		a[i]=temp;		
+		temp=as[j];
+		as[j]=as[i];
+		as[i]=temp;		
 		}	
 	}
 	}
